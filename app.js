40399c8fec0703a574a1a3a2540ca0a54124379a
SurenderLohia <surender.lohia.e@gmail.com>
2020-09-14 22:12:50 +0530

    Update cors url

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://top-links.surge.sh",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
